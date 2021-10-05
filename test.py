5ef9f468815df10fb264abea55c45a1088efa6eb
pan-dya <pandya.limawan@gmail.com>
2021-10-05 17:24:46 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+number_1 = eval(input("Enter the first number:"))
+number_2 = eval(input("Enter the second number:"))
+
+question= input('Do you want to \n A. + \n B. X \n C. / \n D. - \n [A/B/C/D] ?')
+if question == "A" :
+    print(number_1 + number_2)
+elif question == "B" :
+    print(number_1 * number_2)
+elif question == "C" :
+    print (number_1 - number_2)
+else :
+    print("error!")
