5ebd644c31db6e4008d166edc2a091e70246ffbc
pan-dya <86063345+pan-dya@users.noreply.github.com>
2021-10-11 15:07:13 +0700

    Delete exercise 1 directory

--- exercise 1/ex1q1.py
+++
@@ -1,8 +0,0 @@
-x1 = eval(input("Enter the first x coordinate point: "))
-y1 = eval(input("Enter the first y coordinate point: "))
-x2 = eval(input("Enter the second x coordinate point: "))
-y2 = eval(input("Enter the second y coordinate point: "))
-
-slope = ((y2-y1)/(x2-x1))
-formatSlope = round(slope, 5)
-print('the slope for the line that connects two points', (x1, x2), 'and', (y1,y2), 'is', formatSlope)
