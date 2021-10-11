0c867736c94970bdb82ab78522c3622b5d8918c1
pan-dya <86063345+pan-dya@users.noreply.github.com>
2021-10-11 15:07:24 +0700

    Delete exercise 2 directory

--- exercise 2/ex2q2.py
+++
@@ -1,9 +0,0 @@
-e1 = eval(input("Enter length of edge 1: "))
-e2 = eval(input("Enter length of edge 2: "))
-e3 = eval(input("Enter length of edge 3: "))
-perimeter = e1 + e2 + e3
-
-if (e1 + e2) < e3 or (e1 + e3) < e2 or (e2+e3) < e1:
-    print ("Perimeter cannot be calculated")
-else:
-    print ('The perimeter is', perimeter)
